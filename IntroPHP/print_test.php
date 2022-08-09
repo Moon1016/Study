baca1785fbf9c72133a815b1ae93b71a4d2746bc
Moon1016 <109051985+Moon1016@users.noreply.github.com>
2022-08-09 12:48:03 +0900

    Add files via upload

---
+++ IntroPHP/print_test.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello HTML
+    Hello HTML
+    <hr>
+    <script>
+        document.write("Hello Javascript ");
+        document.write("Hello Javascript <hr>");
+        </script>
+        <?php
+        print("Hello PHP ");
+        print("Hello PHP <hr>");
+        echo "Hello PHP";
+        echo "<hr>";
+        $sum = 1 + 2;
+        echo $sum;
+        echo "<hr>";
+        $age = 25;
+        $blood_type = "O";
+        $name = "이영민";
+
+        print "나이 : $age <br>";
+        print "혈액형 : $blood_type <br>";
+        print "이름 : $name <br>";
+        
+        ?>
+</body>
+</html>
