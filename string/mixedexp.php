df64687d7af9e4f062566be49926ffd8df394499
Moon1016 <109051985+Moon1016@users.noreply.github.com>
2022-08-18 12:47:42 +0900

    Add files via upload

---
+++ string/mixedexp.php
@@ -0,0 +1,27 @@
+<?php
+$i = 1;
+$f = 1.1;
+$str = "string";
+$b = true;
+print var_dump($i)."<br>";
+print var_dump($f)."<br>";
+print var_dump($str)."<br>";
+print var_dump($b)."<br>";
+print var_dump($i+1)."<br>";
+print var_dump($f+1)."<br>";
+print var_dump($str."01")."<br>";
+print var_dump($b && false)."<br>";
+
+print "<hr>";
+
+$str1 = "http://www.php.edu/testurl1.html?name=kim&age=28&def=aaa";
+$str2 = parse_url($str1);
+var_dump($str2);
+print "<br>";
+parse_str($str2["query"], $abc);
+print_r($abc);
+print "<br>";
+print "이름 : " . $abc["name"] . "<br>";
+print "나이 : " . $abc["age"] . "<br>";
+print "기타 : " . $abc["def"] . "<br>";
+?>
